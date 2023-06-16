b8a946f0326b5cef1d5fa60723941c5de635bfe3
alumnoIdat <lwmoralesc@uni.pe>
2023-06-16 06:00:35 +0000

    "Basic Node and Express" finished

--- myApp.js
+++ myApp.js
@@ -1,5 +1,54 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+
+console.log("Hello World");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+})
+
+app.get("/", function(req, res) {
+  var absolutePath = `${__dirname}/views/index.html`;
+  res.sendFile(absolutePath);
+})
+
+app.use("/public", function(req, res) {
+  var absolutePath = `${__dirname}/public/style.css`;
+  res.sendFile(absolutePath);
+})
+
+app.get("/json", function(req, res) {
+  var response = "";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = "Hello json".toUpperCase();
+  } else {
+    response = "Hello json";
+  }
+  var jsonRes = { "message": response };
+  res.json(jsonRes);
+})
+
+app.get("/now", function(req, res, next) {
+  req.time = new Date().toString();
+  next()
+}, function(req, res) {
+  res.json({time: req.time})
+})
+
+app.get("/:word/echo", function(req, res) {
+  res.json({ echo: req.params.word })
+})
+
+app.get("/name", function(req, res) {
+  res.json({ name: `${req.query.first} ${req.query.last}` });
+})
+
+app.post("/name", function(req, res) {
+  res.json({ name: `${req.body.first} ${req.body.last}` })
+})
 
 
 
@@ -17,23 +66,4 @@ let app = express();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
